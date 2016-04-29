3dd80b2b68ff41dc5a1ad87c38c3c406842365ba
Corey Frenette <corey@narwhunderful.com>
2016-04-29 17:41:19 -0300

    Unit tests

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -2,14 +2,19 @@
 
 use BapCat\Phi\Phi;
 use BapCat\Hashing\HashingServiceProvider;
+use BapCat\Hashing\FastHash;
 use BapCat\Hashing\FastHasher;
+use BapCat\Hashing\WeakHash;
 use BapCat\Hashing\WeakHasher;
+use BapCat\Hashing\StrongHash;
 use BapCat\Hashing\StrongHasher;
+use BapCat\Hashing\PasswordHash;
 use BapCat\Hashing\PasswordHasher;
 use BapCat\Hashing\Algorithms\Crc32FastHasher;
 use BapCat\Hashing\Algorithms\Sha1WeakHasher;
 use BapCat\Hashing\Algorithms\Sha256StrongHasher;
 use BapCat\Hashing\Algorithms\BcryptPasswordHasher;
+use BapCat\Values\Password;
 
 class ServiceProviderTest extends PHPUnit_Framework_TestCase {
   private $ioc;
@@ -32,5 +37,10 @@ class ServiceProviderTest extends PHPUnit_Framework_TestCase {
     $this->assertSame($this->ioc->make(WeakHasher::class),     $this->ioc->make(WeakHasher::class));
     $this->assertSame($this->ioc->make(StrongHasher::class),   $this->ioc->make(StrongHasher::class));
     $this->assertSame($this->ioc->make(PasswordHasher::class), $this->ioc->make(PasswordHasher::class));
+    
+    $this->assertTrue($this->ioc->make(FastHash::class, [hash('crc32', 'test')])->check('test'));
+    $this->assertTrue($this->ioc->make(WeakHash::class, [hash('sha1', 'test')])->check('test'));
+    $this->assertTrue($this->ioc->make(StrongHash::class, [hash('sha256', 'test')])->check('test'));
+    $this->assertTrue($this->ioc->make(PasswordHash::class, [password_hash('testtest', PASSWORD_BCRYPT)])->check(new Password('testtest')));
   }
 }
